0704f71bf27abafb1bfe08e21b36f0c2a37a26a4
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-21 16:23:11 +0100

    Update kite.rb

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,13 +1,13 @@
 class Kite < Formula
   desc "A Connect Four solver"
   homepage "https://github.com/tristan852/kite"
-  version "1.16.3"
+  version "1.17.0"
 
   if OS.mac?
-    url "https://github.com/tristan852/kite/releases/download/v1.16.3/kite-1.16.3-macos-x64.tar.gz"
+    url "https://github.com/tristan852/kite/releases/download/v1.17.0/kite-1.17.0-macos-x64.tar.gz"
     sha256 "DC7B999DCD1E6B2DD58D4B33B33CDF24F9ECBC9DA8B44756241AF5122F60C125"
   elsif OS.linux?
-    url "https://github.com/tristan852/kite/releases/download/v1.16.3/kite-1.16.3-linux-x64.tar.gz"
+    url "https://github.com/tristan852/kite/releases/download/v1.17.0/kite-1.17.0-linux-x64.tar.gz"
     sha256 "F209C0454EF5EA37E04E2AC34A828E34B45B4EE5AA1F39D5DAF45D3278569738"
   end
 
@@ -17,6 +17,6 @@ class Kite < Formula
 
   test do
     output = shell_output("#{bin}/kite --version")
-    assert_match "1.16.3", output
+    assert_match "1.17.0", output
   end
 end
