ecf8d5a1599e6c0d7c1e7d148694d5304df14c1f
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-02-27 11:41:34 +0100

    Update kite.rb

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,14 +1,14 @@
 class Kite < Formula
   desc "A Connect Four solver"
   homepage "https://github.com/tristan852/kite"
-  version "1.19.0"
+  version "1.19.1"
 
   if OS.mac?
-    url "https://github.com/tristan852/kite/releases/download/v1.19.0/kite-1.19.0-macos-x64.tar.gz"
-    sha256 "6547704C3A2E27D16B0BAFA677449036C7A7A2BC0EF1C995BAA7B0CC518E7198"
+    url "https://github.com/tristan852/kite/releases/download/v1.19.1/kite-1.19.1-macos-x64.tar.gz"
+    sha256 "BD9CA6A72ABA777E8A06ED7B30BD5B6346A8A51DA11AB4BACA397E91E7182362"
   elsif OS.linux?
-    url "https://github.com/tristan852/kite/releases/download/v1.19.0/kite-1.19.0-linux-x64.tar.gz"
-    sha256 "881F6B0A601CF5015003740E80D8F4CA1F787933716145BF5715D89039CA0C00"
+    url "https://github.com/tristan852/kite/releases/download/v1.19.1/kite-1.19.1-linux-x64.tar.gz"
+    sha256 "EE0CC5165489C1732305659395FCE54FC2B88CDB3A1787D1BF59913A4ADE9A29"
   end
 
   def install
@@ -17,6 +17,6 @@ class Kite < Formula
 
   test do
     output = shell_output("#{bin}/kite --version")
-    assert_match "1.19.0", output
+    assert_match "1.19.1", output
   end
 end
